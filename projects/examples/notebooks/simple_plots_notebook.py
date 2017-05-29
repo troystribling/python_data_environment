993318365d5db31cd8bc38b6a2a08f11108413d2
Troy Stribling <troy.stribling@gmail.com>
2017-05-29 10:16:25 -0700

    better search path update

--- projects/examples/notebooks/simple_plots_notebook.py
+++ projects/examples/notebooks/simple_plots_notebook.py
@@ -1,6 +1,7 @@
-import os
-import sys
-sys.path.append(os.getcwd() + '/examples')
+import sys, os
+local_lib_path = os.getcwd() + '/examples'
+if local_lib_path not in sys.path:
+    sys.path.append(local_lib_path)
 import simple_plots
 
 simple_plots.line_plot()
