f43a7180878da4b296c1f64f5f9ab55417ddf97e
Troy Stribling <troy.stribling@gmail.com>
2017-06-03 16:24:22 -0700

    add normal distribution methods

--- examples/hyposthesis_testing.py
+++ examples/hyposthesis_testing.py
@@ -1,1 +1,20 @@
 from scipy.stats import norm
+import math
+import numpy
+
+def binomial_to_normal(p, n):
+    μ = n * p
+    σ = numpy.sqrt(n * p (1 - p))
+    return μ, σ
+
+def normal_cdf(x, μ, σ):
+    return norm.cdf(x, μ, math.pow(σ, 2))
+
+def normal_tail(x, μ, σ):
+    return 1.0 - normal_cdf(x, μ, σ)
+
+def normal_interval(x, y, μ, σ):
+    return normal_cdf(y, μ, σ) - normal_cdf(x, μ, σ)
+
+def normal_outside_interval(x, y, μ, σ):
+    return 1.0 - normal_interval(x, y, μ, σ)
