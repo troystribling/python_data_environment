da4a438bd346312a2bc54c8849329fa5fb491082
troystribling <troy.stribling@gmail.com>
2017-06-09 11:59:05 -0700

    added beta distribution baysian posterior example

--- notebooks/beta_distibution_notebook.py
+++ notebooks/beta_distibution_notebook.py
@@ -8,6 +8,7 @@
 from scipy.stats import beta
 
 from examples import stats_plots
+from examples import bernoulli_trials
 
 # The beta distribution is the continuos version of the binomial distribition where factorials are replaced by Γ(n)
 # and the exponents for polynomials are not required to be integers. It is also only defined for 0 <= p <= 1 Where
@@ -33,14 +34,43 @@ beta.cdf(0.0, 2.75, 3.83)
 # ppf is percentile point function which returns the value of the distribution at the specified percentile the
 # inverse of the cdf
 beta.ppf(0.5, 0.5, 1.75)
+beta.ppf(0.5, 1.0, 1.0)
 
 # %%
 x = numpy.linspace(0.0, 1.0, 100)
-beta_pdfs = [beta.pdf(x, 0.5, 0.5), beta.pdf(x, 1.5, 2.75), beta.pdf(x, 2.5, 3.75), beta.pdf(x, 1.0, 1.0),
+beta_pdfs = [beta.pdf(x, 1.0, 1.0), beta.pdf(x, 1.5, 2.75), beta.pdf(x, 8.5, 3.75),
              beta.pdf(x, 10.0, 10.0)]
 stats_plots.multi_line_pdf_plot(x, beta_pdfs)
 
 # %%
 x = numpy.linspace(0.0, 1.0, 100)
-beta_cdfs = [beta.cdf(x, 1.5, 2.75), beta.cdf(x, 2.5, 3.75), beta.cdf(x, 0.5, 0.5)]
+beta_cdfs = [beta.cdf(x, 1.0, 1.0), beta.cdf(x, 1.5, 2.75), beta.cdf(x, 8.5, 3.75),
+             beta.cdf(x, 10.0, 10.0)]
 stats_plots.multi_line_pdf_plot(x, beta_cdfs)
+
+# %%
+# The beta distribution is the conjugate prior of the binomial distribution for
+# estimating the succes probability of a trial. The posterior distribution is given by
+
+def beta_posterior(success_count, n, x, α, β):
+    return beta.pdf(x, α + success_count, β + n - success_count)
+
+## %%
+# estimate probability for fair Bernouli trail using beta posterior with unform
+# prior, α=1.0, β=1.0
+p = 0.5
+ns = [10, 100, 1000, 10000]
+x = numpy.linspace(0.0, 1.0, 100)
+success_counts = [bernoulli_trials.success_count_trial(p, n) for n in ns]
+posterior_distributions = [beta_posterior(success_counts[i], ns[i], x, 1.0, 1.0) for i in range(len(ns))]
+stats_plots.multi_line_pdf_plot(x, posterior_distributions)
+
+## %%
+# estimate probability for failing Bernouli trail using beta posterior with unform
+# prior, α=1.0, β=1.0
+p = 0.35
+ns = [10, 100, 1000, 10000]
+x = numpy.linspace(0.0, 1.0, 100)
+success_counts = [bernoulli_trials.success_count_trial(p, n) for n in ns]
+posterior_distributions = [beta_posterior(success_counts[i], ns[i], x, 1.0, 1.0) for i in range(len(ns))]
+stats_plots.multi_line_pdf_plot(x, posterior_distributions)
