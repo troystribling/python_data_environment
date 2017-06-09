9089b183d5959bf7d1547408386eae02d7b64c35
troystribling <troy.stribling@gmail.com>
2017-06-08 18:31:27 -0700

    added some verbage

--- notebooks/a_b_test_notebook.py
+++ notebooks/a_b_test_notebook.py
@@ -13,7 +13,8 @@ from examples import bernoulli_trials
 # A/B trial test statistic is derived from the the difference of the trial probability estimate
 # s = (μa - μb)/sqrt(σa**2 + σb**2) which is assumed to be normally distributed with mean 0 and
 # varaince 1 as a consequence of the bernoulli trial having a binomial distribution which is approximately
-# normal for large n
+# normal for large n. This statistic is a statement of the null hypothesis for the two trials. The
+# null hypothesis is that the deference between the trails will only be due to its random nature.
 def a_b_test_statistic(μa, σa, μb, σb):
     return (μa - μb)/numpy.sqrt(σa**2 + σb**2)
 
@@ -29,6 +30,9 @@ success_count_trial_b = bernoulli_trials.success_count_trial(pb, nb)
 μb, σb = bernoulli_trials.params_estimate_probability(success_count_trial_b, nb)
 
 test_stat = a_b_test_statistic(μa, σa, μb, σb)
+
+# verify that the null hypothesis is staisified by verifyting that the result is within the 95%
+# confidence interval.
 bernoulli_trials.pvalue(test_stat, 0.0, 1.0)
 
 # %%
@@ -45,4 +49,7 @@ success_count_trial_b = bernoulli_trials.success_count_trial(pb, nb)
 μb, σb = bernoulli_trials.params_estimate_probability(success_count_trial_b, nb)
 
 test_stat = a_b_test_statistic(μa, σa, μb, σb)
+
+# verify that the null hypothesis is staisified by verifyting that the result is within the 95%
+# confidence interval.
 bernoulli_trials.pvalue(test_stat, 0.0, 1.0)
