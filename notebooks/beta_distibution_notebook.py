26c5a41cbe8f958903351c8c3642ea3cebdc74ef
Troy Stribling <troy.stribling@gmail.com>
2017-06-11 12:51:38 -0700

    finished beta distribution exaples

--- notebooks/beta_distibution_notebook.py
+++ notebooks/beta_distibution_notebook.py
@@ -19,6 +19,22 @@ from examples import bernoulli_trials
 #   Binomial(p, n, m) = [(x^m)(1 - x)^(n-m)]n!/(n-m)!m!
 
 # %%
+# The beta distribution is the conjugate prior of the binomial distribution for
+# estimating the succes probability of a trial. The posterior distribution is given by
+
+def beta_posterior(success_count, n, x, α, β):
+    return beta.pdf(x, α + success_count, β + n - success_count)
+
+def beta_μ(α, β):
+    return α/(α + β)
+
+def beta_σ(α, β):
+    return numpy.sqrt(α * β/((α + β + 1)*(α + β)**2))
+
+def beta_mode(α, β):
+    return (α - 1.0) / (α + β - 2)
+
+# %%
 # cdf is the cumlative distribution function
 # For standard beta distribution the cdf approaches 1 as x ⇢ 1
 beta.cdf(1.0, 0.5, 1.75)
@@ -32,41 +48,128 @@ beta.cdf(0.0, 2.75, 3.83)
 
 # %%
 # ppf is percentile point function which returns the value of the distribution at the specified percentile the
-# inverse of the cdf
+# inverse of the cdf. No close for expression exists for the median of the Beta distribution so it must be evaluated
+# numerically using the percentile point function.
 beta.ppf(0.5, 0.5, 1.75)
 beta.ppf(0.5, 1.0, 1.0)
 
 # %%
+beta_μ(3.0, 5.0)
+beta.mean(3.0, 5.0)
+
+beta_σ(3.0, 5.0)
+beta.std(3.0, 5.0)
+
+# %%
+# The Beta distribution is unform for α = β = 1.
 x = numpy.linspace(0.0, 1.0, 100)
-beta_pdfs = [beta.pdf(x, 1.0, 1.0), beta.pdf(x, 1.5, 2.75), beta.pdf(x, 8.5, 3.75), beta.pdf(x, 10.0, 10.0)]
+stats_plots.pdf_plot(x, beta.pdf(x, 1.0, 1.0))
+
+# %%
+# For α > 1 and β > 1 the Beta distribution has a maximum mode for 0 < x < 1
+beta_mode(2.0, 4.0)
+beta_mode(5.5, 5.5)
+beta_mode(10.5, 5.5)
+x = numpy.linspace(0.0, 1.0, 100)
+beta_pdfs = [beta.pdf(x, 2.0, 4.0), beta.pdf(x, 5.5, 5.5), beta.pdf(x, 10.5, 5.5)]
 stats_plots.multi_line_pdf_plot(x, beta_pdfs)
 
 # %%
-x = numpy.linspace(0.0, 1.0, 100)
-beta_pdfs = [beta.pdf(x, 0.1, 0.1), beta.pdf(x, 0.5, 0.5), beta.pdf(x, 0.75, 0.75), beta.pdf(x, 0.9, 0.9)]
+# For  1 < α < 2 and 0 < β < 2 - α no exterma exists for 0 < x < 1. The maximum will be at and end point.
+x = numpy.linspace(0.0, 1.0, 1000)
+beta_pdfs = [beta.pdf(x, 1.5, 0.1), beta.pdf(x, 1.5, 0.25), beta.pdf(x, 1.5, 0.45)]
 stats_plots.multi_line_pdf_plot(x, beta_pdfs)
 
 # %%
-x = numpy.linspace(0.0, 1.0, 100)
-beta_pdfs = [beta.pdf(x, 0.1, 0.001), beta.pdf(x, 0.25, 0.75), beta.pdf(x, 0.5, 0.75)]
-stats_plots.multi_line_pdf_plot(x, beta_pdfs)
-# %%
-x = numpy.linspace(0.0, 1.0, 100)
-beta_pdfs = [beta.pdf(x, 1.0, 3.0), beta.pdf(x, 3.0, 1.0)]
+# For  α < 1 and 1 < β < 2 - α no exterma exists for 0 < x < 1. The maximum will be at and end point.
+x = numpy.linspace(0.0, 1.0, 1000)
+beta_pdfs = [beta.pdf(x, 0.5, 1.0), beta.pdf(x, 0.5, 1.25), beta.pdf(x, 0.5, 1.45)]
 stats_plots.multi_line_pdf_plot(x, beta_pdfs)
 
 # %%
+# For  α < 1 and β > 2 exterma exists for 0 < x < 1. The maximum will be at and end point.
+x = numpy.linspace(0.0, 1.0, 1000)
+beta_pdfs = [beta.pdf(x, 0.5, 5.0), beta.pdf(x, 0.5, 10.0), beta.pdf(x, 0.5, 20.0)]
+stats_plots.multi_line_pdf_plot(x, beta_pdfs)
+
+# %%
+# For  α > 1 and β < 1 exterma exists for 0 < x < 1. The maximum will be at and end point.
+x = numpy.linspace(0.0, 1.0, 1000)
+beta_pdfs = [beta.pdf(x, 5.0, 0.5), beta.pdf(x, 10.0, 0.5), beta.pdf(x, 20.0, 0.5)]
+stats_plots.multi_line_pdf_plot(x, beta_pdfs)
+
+# %%
+# For  α < 1 and β < 1 the mode is a minimm or antimode
+beta_mode(0.5, 0.1)
+beta_mode(0.5, 0.5)
+beta_mode(0.5, 0.9)
+x = numpy.linspace(0.0, 1.0, 1000)
+beta_pdfs = [beta.pdf(x, 0.5, 0.1), beta.pdf(x, 0.5, 0.5), beta.pdf(x, 0.5, 0.9)]
+stats_plots.multi_line_pdf_plot(x, beta_pdfs)
+
+# %%
+# For  α >> β and β > 1 the mode approaches 1
+beta_mode(5.0, 1.5),
+beta_mode(20.0, 1.5)
+beta_mode(50.0, 1.5)
+x = numpy.linspace(0.0, 1.0, 1000)
+beta_pdfs = [beta.pdf(x, 5.0, 1.5), beta.pdf(x, 20.0, 1.5), beta.pdf(x, 50.0, 1.5)]
+stats_plots.multi_line_pdf_plot(x, beta_pdfs)
+
+# %%
+# For  β >> α and α > 1 the mode approaches 0
+beta_mode(1.5, 5.0)
+beta_mode(1.5, 20.0)
+beta_mode(1.5, 50.0)
+x = numpy.linspace(0.0, 1.0, 1000)
+beta_pdfs = [beta.pdf(x, 1.5, 5.0), beta.pdf(x, 1.5, 20.0), beta.pdf(x, 1.5, 50.0)]
+stats_plots.multi_line_pdf_plot(x, beta_pdfs)
+
+# %%
+# For  α >> β and β > 1 the mode approaches 1
+beta_mode(5.0, 0.5)
+beta_mode(20.0, 0.5)
+beta_mode(50.0, 0.5)
+x = numpy.linspace(0.0, 1.0, 1000)
+beta_pdfs = [beta.pdf(x, 5.0, 0.5), beta.pdf(x, 20.0, 0.5), beta.pdf(x, 50.0, 0.5)]
+stats_plots.multi_line_pdf_plot(x, beta_pdfs)
+
+# %%
+# For  α = β and α, β > 1 the distribution is symetric about the mode and becomes sharper as α increases.
+beta_mode(5.0, 5.0)
+beta_mode(20.0, 20.0)
+beta_mode(50.0, 50.0)
+beta_μ(5.0, 5.0)
+beta_μ(20.0, 20.0)
+beta_μ(50.0, 50.0)
+beta_σ(5.0, 5.0)
+beta_σ(20.0, 20.0)
+beta_σ(50.0, 50.0)
+x = numpy.linspace(0.0, 1.0, 1000)
+beta_pdfs = [beta.pdf(x, 5.0, 5.0), beta.pdf(x, 20.0, 20.0), beta.pdf(x, 50.0, 50.0)]
+stats_plots.multi_line_pdf_plot(x, beta_pdfs)
+
+# %%
+# For  α = β and α, β < 1 the mode approaches 1
+beta_mode(0.05, 0.05)
+beta_mode(0.1, 0.1)
+beta_mode(0.5, 0.5)
+beta_μ(0.05, 0.05)
+beta_μ(0.1, 0.1)
+beta_μ(0.5, 0.5)
+beta_σ(0.05, 0.05)
+beta_σ(0.1, 0.1)
+beta_σ(0.5, 0.5)
+x = numpy.linspace(0.0, 1.0, 1000)
+beta_pdfs = [beta.pdf(x, 0.05, 0.05), beta.pdf(x, 0.1, 0.1), beta.pdf(x, 0.5, 0.5)]
+stats_plots.multi_line_pdf_plot(x, beta_pdfs)
+
+# %%
+# Example CDFs
 x = numpy.linspace(0.0, 1.0, 100)
 beta_cdfs = [beta.cdf(x, 1.0, 1.0), beta.cdf(x, 1.5, 2.75), beta.cdf(x, 8.5, 3.75),
              beta.cdf(x, 10.0, 10.0)]
 stats_plots.multi_line_pdf_plot(x, beta_cdfs)
-
-# %%
-# The beta distribution is the conjugate prior of the binomial distribution for
-# estimating the succes probability of a trial. The posterior distribution is given by
-
-def beta_posterior(success_count, n, x, α, β):
-    return beta.pdf(x, α + success_count, β + n - success_count)
 
 ## %%
 # estimate probability for fair Bernouli trail using beta posterior with unform
