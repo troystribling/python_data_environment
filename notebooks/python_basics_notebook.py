6dcd6e2efb6d049df9df1e74b7d40dd5f4f52388
Troy Stribling <troy.stribling@gmail.com>
2017-07-01 13:57:13 -0700

    added decorator examples

--- notebooks/python_basics_notebook.py
+++ notebooks/python_basics_notebook.py
@@ -14,6 +14,19 @@ def call_foo(foofoo, arg):
 call_foo(foo, 3)
 
 # %%
+# function arguments
+def foo_args(*args):
+    print(f"args: {args}")
+
+
+foo_args(1,2,3)
+
+def foo_kwargs(**kwargs):
+    print(f"kwargs: {kwargs}")
+
+foo_kwargs(city="SF", name="Troy", zip=94110)
+
+# %%
 # returning a function
 
 def parent(num):
@@ -36,7 +49,6 @@ def my_decorator(f):
         print("Before function")
         f()
         print("After function")
-
     return wrapper
 
 def a_function():
@@ -68,10 +80,37 @@ some_other_function()
 
 # %%
 # decorator with arguments
-def deorator_with_args(arg):
+def decorator_with_args(*outer_args):
     def decorator(f):
-        print("arg: %(arg)" % locals())
-        
+        print(f"decorator outer_args: {outer_args}")
+        def wrapper(*args):
+            print(f"wrapper outer_args: {outer_args}, args: {args}")
+            return f(*args)
+        return wrapper
+    return decorator
+
+@decorator_with_args(1,2,3)
+def process_args(*args):
+    print(f"process_args: {args}")
+
+process_args(4,5,6)
+
+def wraps_decorator_with_args(*outer_args):
+    def decorator(f):
+        print(f"wraps_decorator outter_args: {outer_args}")
+        @wraps(f)
+        def wrapper(*args):
+            print(f"wraps_wrapper outter_args: {outer_args}, args: {args}")
+            return f(*args)
+        return wrapper
+    return decorator
+
+@wraps_decorator_with_args(7,8,9)
+def wraps_process_args(*args):
+    print(f"wraps_process_args: {args}")
+
+wraps_process_args(10,11,12)
+
 # %%
 # class example
 class Thing(object):
@@ -82,7 +121,7 @@ thing = Thing(5)
 thing.value
 
 # %%
-# simple class decorator
+# class decorator
 class Pizza(object):
     def __init__(self):
         self.toppings = []
@@ -91,6 +130,15 @@ class Pizza(object):
         self.toppings.append(topping())
     def __repr__(self):
         return str(self.toppings)
+    def deliver(self, vendor):
+        def decorator(f):
+            print(f"pizza delivered by: {vendor}")
+            @wraps(f)
+            def wrapper(*args):
+                return f(*args)
+            return wrapper
+        return decorator
+
 
 pizza = Pizza()
 
@@ -103,3 +151,9 @@ def sauce():
     return 'sauce'
 
 print(pizza)
+
+@pizza.deliver('Dominos')
+def delivered(*args):
+    print(f"delivered: {args}")
+
+delivered(10, 3)
